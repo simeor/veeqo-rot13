9e548a7ea387029e5b616c985ad0d0f95ba135c4
simeo russo <simeo94@hotmail.com>
2020-09-28 15:08:51 +0100

    first push

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+This project was created with Ruby On Rails
+
+
+Available Scripts
+
+In the project (veeqo-rot13), you can run in the terminal:
+`rails s`
+
+Runs the app  development server .
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+If error running `rails s` try running `bundle install`
+
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
-  def home
+
+def home
+  def rot_cipher(input, value)
+    if input.size > 0 && ( value <= 25 && value > 0)
+      lower_case = calculate(input, value)
+      calculate(lower_case.swapcase, value).swapcase
+    else
+      return " ⚠️ ️Please enter a string and number between 1 and 25"
+    end
   end
+
+  def calculate(input, value)
+    #create an array of letters a-z
+    alphabet = ("a".."z").to_a
+    # split alph array on input number
+    alph_a , alph_b = alphabet.take(value) , alphabet.drop(value)
+    # interpolate for the tr. range
+    search = "#{alph_b.first}-#{alph_b.last}#{alph_a.first}-#{alph_a.last}"
+    input.tr("a-z", search)
+    end
+  end
+  helper_method :rot_cipher
 end
